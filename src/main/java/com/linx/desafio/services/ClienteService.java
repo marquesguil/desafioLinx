037333db11950c89bef08a304c8a5fe2ae9522da
Guilherme <marquesguil@gmail.com>
2020-10-18 23:50:10 -0300

    Classes de servico e repository criadas e rest testado

---
+++ src/main/java/com/linx/desafio/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.linx.desafio.services;
+
+import com.linx.desafio.domain.Cliente;
+import com.linx.desafio.exception.ObjectNotFoundException;
+import com.linx.desafio.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id) throws ObjectNotFoundException {
+        Optional<Cliente> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+    }
+
+
+}
