ae6aa1342dcb44bdc5c7e102d3e219d724b4944c
Guilherme <marquesguil@gmail.com>
2020-10-20 01:00:04 -0300

    Projeto finalizado

--- src/main/java/com/linx/desafio/DesafioApplication.java
+++ src/main/java/com/linx/desafio/DesafioApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DesafioApplication implements CommandLineRunner {
 
-	private ClienteRepository clienteRepository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioApplication.class, args);
 	}
